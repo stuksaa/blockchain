4f0e643f1d495a04bb128101e20ffb2e44127168
stuksaa <stuksaa@gmail.com>
2019-12-17 16:12:20 +0100

    Addig transaction class.

---
+++ transaction.py
@@ -0,0 +1,15 @@
+
+class Transaction():
+
+	def __init__(self, amount, senderKey, receiverKey, timestamp):
+		self.amount = amount
+		self.senderKey = senderKey
+		self.receiverKey = receiverKey
+		self.timestamp = timestamp
+		
+	def __str__(self):
+		return( 'Amount: ' + str(self.amount) + '\n' +
+				'senderKey: ' + str(self.senderKey) + '\n' +
+				'receiverKey: ' + str(self.receiverKey) + '\n' +
+				'timestamp: ' + str(self.timestamp) ) 
+		
